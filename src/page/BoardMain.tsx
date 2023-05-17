8eb5eb866a3ebee8b9f9f9b7405b658af2e1a2eb
pajiyeee <jijiji941@gmail.com>
2023-05-17 23:14:21 +0900

    Add: 게시판리스트 필터구현중-큰일 벌이기 전

--- src/page/BoardMain.tsx
+++ src/page/BoardMain.tsx
@@ -4,7 +4,6 @@ import { Center, Heading } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 import { useRecoilState } from 'recoil';
 import { listState } from '../recoil/listState';
-// import { limitState } from '../recoil/limitState';
 import BoardItem from '../Components/BoardMain/BoardItem';
 import Pagination from '../Components/BoardMain/Pagination';
 import BoardListTitle from '../Components/BoardMain/BoardListTitle';
@@ -13,9 +12,9 @@ import SelectLimit from '../Components/BoardMain/SelectLimit';
 const BoardMain = () => {
   const navigate = useNavigate();
   const [boardList] = useRecoilState(listState);
+  const totalItem = boardList.length;
   const [page, setPage] = useState(1);
-  // const [limit, setLimit] = useRecoilState(limitState);
-  const [limit, setLimit] = useState(boardList.length);
+  const [limit, setLimit] = useState(totalItem);
   const offset = (page - 1) * limit;
 
   const goToDetail = () => {
@@ -29,12 +28,16 @@ const BoardMain = () => {
             <Heading>게시판리스트</Heading>
             <WriteButton onClick={goToDetail}>글쓰기</WriteButton>
           </TitleWrap>
-          <SelectLimit totalItem={boardList.length} setLimit={setLimit} />
+          <SelectLimit totalItem={totalItem} setLimit={setLimit} />
           <BoardListTitle />
-          {boardList.slice(offset, offset + limit).map((item) => (
-            <BoardItem key={item.id} item={item} />
-          ))}
-          <Pagination totalItem={boardList.length} limit={limit} page={page} setPage={setPage} />
+          {totalItem >= 1 && limit === totalItem
+            ? boardList.map((item) => <BoardItem key={item.id} item={item} />)
+            : boardList
+                .slice(offset, offset + limit)
+                .map((item) => <BoardItem key={item.id} item={item} />)}
+          {totalItem >= 1 && (
+            <Pagination totalItem={totalItem} limit={limit} page={page} setPage={setPage} />
+          )}
         </BoardMainWrap>
       </Center>
     </>
@@ -64,3 +67,27 @@ const WriteButton = styled.button`
   background-color: #222222;
   color: #ffffff;
 `;
+
+/*
+
+SelectLimit.tsx
+state 로 관리 
+
+
+const changeSelect = () => {
+  setSelect(e.target.value)
+}
+
+<select onChange={changeSelect}/>
+ <option value="all" />
+ <option value={5} />
+
+---------------------------------------------------
+ Main.tsx
+const [select, setSelect] = useRecoilState(select)
+
+const select === 'all' 
+slice 없이 map
+근데 'all' 을 어떻게 boardList.length라는 것을 명시해줄지....
+{all : boardList.length,five:5}
+*/
